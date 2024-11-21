753ed320d14b2d40ae85ed8f338fde6703f5cba5
Flavien74 <107756028+Flavien74@users.noreply.github.com>
2024-11-21 11:27:01 +0100

    [ADD] new font

--- src/Game/Resources.h
+++ src/Game/Resources.h
@@ -16,15 +16,15 @@ namespace Resources
 	static constexpr float MinSpriteSpeed = 50;
 	static constexpr float MaxSpriteSpeed = 150;
 	
-	static constexpr auto FontPath = "res/Holen Vintage.otf";
-	static constexpr auto FontSize = 12;
+	static constexpr auto FontPath = "res/LeagueSpartan-Bold.otf";
+	static constexpr auto FontSize = 15;
 
 	static constexpr float MinIntervalBetweenSpawns = 0.2f;
 	static constexpr float MaxIntervalBetweenSpawns = 1.f;
 	static constexpr float TimeToReachMinInterval = 60.f;
 
 	static constexpr MyColor BackgroundColor = {15, 15, 15};
-	static constexpr MyColor OnScreenTextColor = {255, 255, 255, 127};
+	static constexpr MyColor OnScreenTextColor = {255, 255, 255, 200};
 	static constexpr Vec2 FpsTextPosition = {10, 10};
 	static constexpr Vec2 ScoreTextPosition = {10, 25};
 	static constexpr float FpsUpdateInterval = 1.f;
